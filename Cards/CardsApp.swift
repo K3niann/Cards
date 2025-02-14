58349135fc5d744e96395fb186bdb554487c6d2f
Keniann Sierra <k3niann@gmail.com>
2025-02-13 19:00:56 -0500

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Keniann Sierra on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
